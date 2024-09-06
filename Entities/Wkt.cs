057a7821f8e422f5a462013b4063a2d95ed9b30f
frknkc <00kocfurkan@gmail.com>
2024-09-06 12:21:30 +0300

    wkt class updated

--- Entities/Wkt.cs
+++ Entities/Wkt.cs
@@ -4,5 +4,7 @@
     {
         public long Id { get; set; }
         public string? WktString { get; set; }
+
+        public string? Name { get; set; }
     }
 }
---
+++ Migrations/20240903134819_mig2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BasarsoftInternship.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Wkts",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Wkts");
+        }
+    }
+}
